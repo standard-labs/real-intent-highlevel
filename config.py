94e139e8ae41f5172db07f67764c2006388c0059
Andrew Lou <andrewloupro@gmail.com>
2025-08-26 20:28:11 -0700

    Added config.py from cincopro app

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+import streamlit as st
+
+load_dotenv()
+
+# For OAuth2.0 authentication
+HIGHLEVEL_AUTH_URL = os.getenv("HIGHLEVEL_AUTH_URL") or st.secrets["HIGHLEVEL_AUTH_URL"]
+CLIENT_ID = os.getenv("CLIENT_ID") or st.secrets["CLIENT_ID"]
+CLIENT_SECRET = os.getenv("CLIENT_SECRET") or st.secrets["CLIENT_SECRET"]
+REDIRECT_URI = os.getenv("REDIRECT_URI") or st.secrets["REDIRECT_URI"]
+
+# For API requests
+HIGHLEVEL_API_URL = os.getenv("HIGHLEVEL_API_URL") or st.secrets["HIGHLEVEL_API_URL"]
