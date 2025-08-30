f7e7ee001bc1ee04251e81b9ce2ce9e744db12fe
Andrew Lou <andrewloupro@gmail.com>
2025-08-30 16:43:47 -0700

    Updated api URL

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ load_dotenv()
 
 # For OAuth2.0 authentication
 HIGHLEVEL_AUTH_URL = os.getenv("HIGHLEVEL_AUTH_URL") or st.secrets["HIGHLEVEL_AUTH_URL"]
-HIGHLEVEL_URL = os.getenv("HIGHLEVEL_URL") or st.secrets["HIGHLEVEL_URL"]
 CLIENT_ID = os.getenv("CLIENT_ID") or st.secrets["CLIENT_ID"]
 CLIENT_SECRET = os.getenv("CLIENT_SECRET") or st.secrets["CLIENT_SECRET"]
 REDIRECT_URI = os.getenv("REDIRECT_URI") or st.secrets["REDIRECT_URI"]
